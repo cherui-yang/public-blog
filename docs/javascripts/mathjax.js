13c8c5b0522e9137ffe900e1fe818aaed2beaa3b
cherui-yang <1197498526@qq.com>
2026-06-02 19:41:58 +0800

    描述更新

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -1,7 +1,7 @@
 window.MathJax = {
   tex: {
-    inlineMath: [["\\(", "\\)"]],
-    displayMath: [["\\[", "\\]"]],
+    inlineMath: [["\\(", "\\)"]],  // 让网页识别行内公式 \(...\)
+    displayMath: [["\\[", "\\]"]], // 让网页识别块级公式 \[...\]
     processEscapes: true,
     processEnvironments: true
   },
@@ -10,3 +10,8 @@ window.MathJax = {
     processHtmlClass: "arithmatex"
   }
 };
+
+// 这个函数确保页面在加载或切换时，新内容中的公式能被正确渲染
+document$.subscribe(() => { 
+  MathJax.typesetPromise()
+})
